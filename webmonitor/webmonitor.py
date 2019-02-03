794fb03d946c893812fba10a867ec8d4725c62db
Mko13 <manana.ko@gmail.com>
2019-02-03 15:49:20 +0200

    Change the sequence of exception handling

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -14,10 +14,10 @@ def checkstatus(url):
         r = requests.get(url, timeout = TIMEOUT)
         r.raise_for_status()
         status = "Page OK."
-    except requests.exceptions.RequestException as request_error:
-        status = request_error
     except requests.exceptions.HTTPError as error:
         status = error
+    except requests.exceptions.RequestException as request_error:
+        status = request_error
         sys.exit(1)
     return r.status_code, status, url
         
