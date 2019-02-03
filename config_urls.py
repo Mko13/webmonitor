937bf3048ce4c6d235ed6f8047412ee1ff76f7e4
Mko13 <manana.ko@gmail.com>
2019-02-03 14:28:58 +0200

    Add list of URLs

--- config_urls.py
+++ config_urls.py
@@ -4,3 +4,7 @@ should begin from a new line. To skip any address, comment it
 by appending a hash sign (#) in the beginning of the line
 """
 
+https://yle.fi/uutiset/osasto/news/
+https://www.aalto.fi/
+https://www.f-secure.com/en_US/welcome
+https://www.greenwichtime.com/
