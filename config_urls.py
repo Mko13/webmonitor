330af25b58bba87a3d9e3bee5ec545646af01c62
Mko13 <manana.ko@gmail.com>
2019-02-03 14:28:05 +0200

    Create configuration file

---
+++ config_urls.py
@@ -0,0 +1,6 @@
+""" This file includes the list of URLs to be monitored. 
+Other URLs can be added by the user as needed. Every entry 
+should begin from a new line. To skip any address, comment it 
+by appending a hash sign (#) in the beginning of the line
+"""
+
