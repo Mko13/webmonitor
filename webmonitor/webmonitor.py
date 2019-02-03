25a2d2dec087988617e0fe675439541799466c76
Mko13 <manana.ko@gmail.com>
2019-02-04 01:44:08 +0200

    Add function for writing logfile

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -44,6 +44,34 @@ def findstring(content, expression):
         return True
     else:
         return False
-    
-    
+
+
 INTERVAL = float(config.get("interval", "request_interval"))
+
+
+# logging module can be used
+def writelog(url_addr):
+    """ Write the status of each webpage in a logfile
+    TO DO: Make cleaner """
+
+    response, status = checkstatus(url_addr)
+    requirement, _parameter = config.items('requirements')[0][:]
+    parameter = _parameter[1:-1]  # remove extra parenthesis
+
+    if not response:
+        response_code = "None"
+        result = "None"
+        time_elapsed = "None"
+    else:
+        response_code = response.status_code
+        result = findstring(response.text, parameter)
+        time_elapsed = response.elapsed
+
+    with open("webmonitor.log", "a+") as logfile:
+        logfile.write("DATE: {} | URL: {} | STATUS: [{}] {} | RESPONSE TIME: {} |\
+                        OTHER INFO: {} '{}': {} \n"
+                        .format(datetime.datetime.now(),
+                                url_addr, response_code, status, time_elapsed,
+                                requirement, parameter, result))
+
+
