114db816afea98fef4d2de2f8d4033651f589059
Mko13 <manana.ko@gmail.com>
2019-02-03 15:37:00 +0200

    Add exception handling

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -8,6 +8,15 @@ import requests
 
 def checkstatus(url):
     """ Make HTTP request to url. return status code """
-    TIMEOUT = 5
-    return requests.get(url, timeout = TIMEOUT)
+    TIMEOUT = 5 # seconds
+    try:
+        r = requests.get(url, timeout = TIMEOUT)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as request_error:
+        print(request_error)
+    except requests.exceptions.HTTPError as error:
+        print(error)
+    return r.status_code
+        #sys.exit(1)
+        
 
